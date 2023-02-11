f77e1ca0efaf29d0ea6616a249be1851e0f7c727
pedrosbhy <pedrosbhy93@gmail.com>
2023-02-10 22:09:40 -0300

    turne em processo

--- Turne/t.c
+++ Turne/t.c
@@ -2,7 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define Item char
+
+
+typedef char Item[27];
+
+#define imprimeedesenfila { printf("%s\n", espia(f)); desenfila(f);}
 
 typedef struct fila_st{
     Item *v;
@@ -12,26 +16,27 @@ typedef struct fila_st{
 
 int inicializa(fila_st *f, int s)
 {
-    f->v = malloc(sizeof(Item)*s);
+    f->v = malloc(sizeof(Item) * s);
     if (f->v == NULL) return 0;
     f->ocupado = 0;
     f->ini = -1;
     f->fim = 0;
+    f->size = s;
     return 1;
 }
 
 int enfila(fila_st *f, Item e)
 {
     if (f->fim == f->ini) return 0;
-    f->v[f->fim++] = e;
+    strcpy(f->v[f->fim++], e);
     f->fim = f->fim % f->size;
     f->ocupado++;
     return 1;
 }
 
-Item espia(fila_st *f)
+Item *espia(fila_st *f)
 {
-    return f->v[(f->ini+1)%f->size];
+    return &f->v[(f->ini+1)%f->size];
 }
 
 int esta_vazia(fila_st *f)
@@ -42,5 +47,49 @@ int esta_vazia(fila_st *f)
 void desenfila(fila_st *f)
 {
     f->ocupado--;
-    f->ini = (f->ini+1)%f->size;
+    f->ini = (f->ini+1) % f->size;
+}
+
+
+char ultimocaratere(Item *v) {
+    while(*(v+1) != 0)
+        v++;
+    return *v[0];
+} 
+
+void coordena(fila_st *f) {
+    int show = 0;
+    char u = ultimocaratere(f->v);
+    imprimeedesenfila;
+
+    for(int i = f->ini + 1; i < f->fim; i++) {
+        if(show) {
+            u = ultimocaratere(f->v[i]);
+            printf("-----%s\n", f->v[i]);
+            imprimeedesenfila;
+            show = 0;
+        }
+        else {
+            if(f->v[0][0] == u) {
+                Item *p = espia(f);
+                u = ultimocaratere(p);
+                imprimeedesenfila;
+                enfila(f, p);
+                show = 1;
+            }
+
+        }
+    }
+}
+
+int main(void) {
+    fila_st f;
+    inicializa(&f, 100000);
+
+    Item palavra;
+    while(scanf("%27s", palavra) == 1)
+        enfila(&f, palavra);
+    
+    coordena(&f);
+    
 }
